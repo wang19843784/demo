33cc870ae611f8aabdc048bb786edbbb97a4d3f1
bjwanglong <bjwanglong@corp.netease.com>
2017-01-19 19:05:02 +0800

    初始化

---
+++ git-demo/src/com/bj/phot/DemoTest.java
@@ -0,0 +1,8 @@
+package com.bj.phot;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+		System.out.println("first--demo");
+	}
+}
--- photo-test/src/com/netease/Photo.java
+++ photo-test/src/com/netease/Photo.java
@@ -2,4 +2,7 @@ package com.netease;
 
 public class Photo {
 
+	public static void main(String[] args) {
+		
+	}
 }
